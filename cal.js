5c541353dd19c37ded2a191db2c454e5fb457dc8
sgtaylor16 <sgtaylor16@gmail.com>
2019-02-16 10:39:38 -0500

    functions working: addBlankMilestone, addCompleteMilestone,deleteMilestone

--- cal.js
+++ cal.js
@@ -11,10 +11,6 @@ function getMaxMilestones(){
     return maxnum;
 }
 
-function scottsTest(){
-    alert("This Worked")
-}
-
 function addBlankMilestone(){
     let newMSnum = getMaxMilestones() + 1;
     let tableElement = document.getElementById("MSTable");
@@ -24,20 +20,59 @@ function addBlankMilestone(){
     //MS Number
     let cell1 = newrow.insertCell(0);
     cell1.innerText = String(newMSnum)
+    cell1.setAttribute("class","MSnumber")
     let cell2 = newrow.insertCell(1);
     cell2.innerHTML = "<input type = 'text' name ='msname' id = 'newms'>"
     let cell3 = newrow.insertCell(2);
     cell3.innerHTML = "<input type = 'Date' name = 'msdate' id = 'newdate'>"
     let cell4 = newrow.insertCell(3);
-    cell4.innerHTML = "<input type = 'Submit'>";
-    cell4.setAttribute("onclick","addCompleteMilestone(c3ll2.textContent,cell3,textContent)");    
+    cell4.innerHTML = "<input type = 'Submit'>";``  
 }
 
 function addCompleteMilestone(comptext,compdate){
     //Set milestone 
-    let milestonecell = getElementById("newms").parentElement;
+    let milestonecell = document.getElementById("newms").parentElement;
     milestonecell.innerHTML = comptext;
     //Set date
-    let datecell = getElementbyID("newdate").parentElement;
+    let datecell = document.getElementById("newdate").parentElement;
     datecell.innerHTML = compdate;
+    //Get rid of the submit button
+    let thisrow = datecell.parentElement
+    thisrow.getElementsByTagName("td")[3].innerHTML = "Null";
+    //add the update column
+    let cell4 = thisrow.insertCell();
+    let inputform = document.createElement("input");
+    let newdateinput = cell4.appendChild(inputform);
+    newdateinput.setAttribute("type","Date");
+    let inputform3 = document.createElement("input");
+    let submitform = cell4.appendChild(inputform3);
+    submitform.setAttribute("type","submit");
+
+    //add the complete button
+    let cell5 = thisrow.insertCell();
+    let inputform2 = document.createElement("input");
+    let newcompleteinput = cell5.appendChild(inputform2);
+    newcompleteinput.setAttribute("type","checkbox");    
+}
+
+function updateDate(tablerow){
+    let mytable = document.getElementById("MSTable");
+    for (onerow of mytable.rows){
+        if (Number(onerow.getElementById("MSnumber").textContent) == tablerow){
+            onerow.getElementById("MSdate").textContent = onerow.getElementsbyTagName()
+        }
+    }
+}
+
+function deleteMilestone(msnumber){
+    let mytable = document.getElementById("MSTable");
+    let myrows = mytable.rows;
+    for (let i=0; i < myrows.length; i++){
+        if (i == 0){continue;}
+        let onerow = myrows[i];
+        let oldnum = onerow.getElementsByClassName("MSnumber")[0].textContent //number of row
+        if (Number(oldnum) == msnumber){
+            mytable.deleteRow(i);
+        }
+    }
 }
